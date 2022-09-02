57953845b0b90f5b55237cac85d2afedca41bf66
zealot83 <zealot33@gmail.com>
2022-09-02 15:45:59 +0900

    커밋 테스트

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
+	// github 연동 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
